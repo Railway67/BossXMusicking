a822bf079d435a2ac78bf282b9387c7f5e5dc852
Railway67 <165446932+Railway67@users.noreply.github.com>
2024-07-21 12:39:19 +0530

    Update config.py
    Signed-off-by: Railway67 <165446932+Railway67@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Railway67/BossXMusic",
+    "https://github.com/Railway67/BossXMusicking",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
